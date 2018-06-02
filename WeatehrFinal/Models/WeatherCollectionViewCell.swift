e8a82114ce90b83dad1e00b03a886839c5101a37
Chin-Fu Yang\FU <asd5242539@gmail.com>
2018-06-03 00:46:46 +0800

    預測的可以動了

---
+++ WeatehrFinal/Models/WeatherCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherCollectionViewCell.swift
+//  WeatehrFinal
+//
+//  Created by FU_MAC on 2018/6/2.
+//  Copyright © 2018年 FU_MAC. All rights reserved.
+//
+
+import UIKit
+
+class WeatherCollectionViewCell: UICollectionViewCell {
+    
+}
